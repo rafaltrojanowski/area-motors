7e9f0470e3bd285d0d4f34b5d91fb271eb6a9f9c
Rafał Trojanowski <rt.trojanowski@gmail.com>
2018-08-12 11:13:14 +0700

    Set root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
-  root to: 'home#index'
-  get "home/index"
+  root to: 'enquiries#index'
+  get "home/index", as: :home
 
 
   resources :enquiries, only: [:index, :show, :update, :edit]
